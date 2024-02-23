53a480e68f4d6440d7cee2c9eb67c5126102a132
MateuszX163 <70938341+MateuszX163@users.noreply.github.com>
2024-02-23 08:08:03 +0100

    a
    a

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Waluty</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="baner1">
+        <img src="waluty.png">
+    </div>
+    <div class="baner2">
+        <h1>Kalkulator walut</h1>
+    </div>
+    <div class="clr">&nbsp;</div>
+    <div class="lewy">
+        <form method="POST" action="index.php">
+            <table>
+                <tr><td><label for="kwota">Kwota:</label></td><td><label for="walutaz">Waluta źródłowa:</label></td><td><label for="walutad">Waluta docelowa:</label></td></tr>
+                <tr><td><input name="kwota" id="kwota" type="text"></td><td><input name="walutaz" id="walutaz" type="text"></td><td><input name="walutad" id="walutad" type="text"></td></tr>
+                <tr>
+                    <td colspan="3"><center><input type="submit"></center></td>
+                </tr>
+
+            </table>
+        </form>
+    </div>
+    <div class="prawy">
+
+    </div>
+
+
+    <script>
+
+    </script>
+</body>
+</html>
